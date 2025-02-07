7856a3347e7e709163e8e35820b5709096f97ce9
orka0928 <h01kz9tim70928@gmail.com>
2025-02-07 13:17:01 +0900

    kadai_008

---
+++ kadi_008.py
@@ -0,0 +1,17 @@
+﻿import random
+
+var = random.randint(3,30)
+
+if var % 3 == 0:
+    print("Fizz")
+
+elif var % 5 == 0:
+    print("Buzz")
+
+elif var % 15 == 0:
+    ("FizzBuzz")
+else:
+    print(var)
+
+
+ 
